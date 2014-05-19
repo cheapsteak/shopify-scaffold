7bf736441d1bcfce2357c8e3cc57b77da85baa2f
Chang Wang <garnwraly@gmail.com>
2014-05-19 16:03:41 -0400

    put in placeholder shopify url

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,7 +137,7 @@ module.exports = function(grunt) {
       options: {
         api_key: process.env.API_KEY,
         password: process.env.PASSWORD,
-        url: "scaffold.myshopify.com",
+        url: "your.shopify.url",
         base: "deploy/"
       }
     },
